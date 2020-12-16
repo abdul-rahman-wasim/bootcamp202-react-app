77da703375e2793c511f1d33439e940e6603267b
KashiRk <67203671+KashiRk@users.noreply.github.com>
2020-12-16 18:10:28 +0500

    Update App.js
    Changed display message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Wokflow Check
+          Wokflow Check abc
         </a>
       </header>
     </div>
