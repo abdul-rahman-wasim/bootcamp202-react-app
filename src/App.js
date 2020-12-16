7b0309f9cb7c60d1d295ab625286040800598348
AtomicVanmaou <64866174+AtomicVanmaou@users.noreply.github.com>
2020-12-16 16:08:47 +0500

    checking workflow

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wokflow Check
         </a>
       </header>
     </div>
